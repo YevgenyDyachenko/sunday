11461c1e352557c54e4a2de563a421d6f96d2eb8
YevgenyDyachenko <yevgeny.dyachenko@gmail.com>
2023-01-15 21:59:29 +0200

    Initial commit

---
+++ src/morning/Day.java
@@ -0,0 +1,11 @@
+package morning;
+
+public class Day {
+    public static void main(String[] args) {
+        System.out.println("It's a good day!!!");
+
+        Sun ok = new Sun();
+        ok.sunny();
+
+    }
+}
---
+++ src/morning/Sun.java
@@ -0,0 +1,12 @@
+package morning;
+
+
+
+public class Sun {
+
+void sunny(){
+    System.out.println("It's sunny today!!!");
+}
+
+
+}
