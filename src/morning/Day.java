c18a07ad5a39cbcfcdf556a648305aa7c60f9030
YevgenyDyachenko <yevgeny.dyachenko@gmail.com>
2023-01-15 23:31:47 +0200

    klo

--- src/morning/Day.java
+++ src/morning/Day.java
@@ -2,7 +2,7 @@ package morning;
 
 public class Day {
     public static void main(String[] args) {
-        System.out.println("It's a good day!!!");
+        System.out.println("It's a good day!!! Uh");
 
         Sun ok = new Sun();
         ok.sunny();
